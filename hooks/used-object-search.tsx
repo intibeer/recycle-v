95b788bb4eb753ab3942485704089772ab5c3b74
Inti Beer <inti@writefull.com>
2024-09-16 16:14:29 +0100

    Adjust columns

--- hooks/used-object-search.tsx
+++ hooks/used-object-search.tsx
@@ -370,7 +370,7 @@ export default function Component() {
                 </DropdownMenuContent>
               </DropdownMenu>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {sortedResults.map((item) => (
                 <Dialog key={item.objectID}>
                   <DialogTrigger asChild>
